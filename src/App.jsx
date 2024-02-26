dc0b69fb163898f192f7fab8425af0af291c2425
Maxi <mmaximonchuk@gmail.com>
2024-02-26 21:40:24 +0200

    new: usestate;

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import MailBox from "./components/MailBox";
 
 import ProductGallery from "./components/ProductGallery/ProductGallery";
@@ -29,16 +30,6 @@ const productData = [
     productName: "Taco S",
     img: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640",
     price: 3.99,
-    quantity: 2,
-    hasDiscount: false,
-    description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, reprehenderit?",
-  },
-  {
-    id: "3_product",
-    productName: "Taco S",
-    img: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640",
-    price: 3.99,
     quantity: 1,
     hasDiscount: false,
     description:
@@ -46,10 +37,53 @@ const productData = [
   },
 ];
 
+const emailsData = [
+  { id: "1", email: "foo@bar.com" },
+  { id: "2", email: "alex1336@bar.com" },
+  { id: "3", email: "maxIm0981@gmail.com" },
+];
+
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [emails, setEmails] = useState(emailsData);
+  const [showMailBox, setShowMailBox] = useState(false);
+
+  const onLogEmail = () => {
+    console.log("Email was sent");
+    setCounter((prevState) => prevState + 1);
+
+    // setCounter(counter + 1) ❌
+    // setCounter((prevState) => prevState + 1) ✅
+  };
+
+  const handleDelete = (mailId) => {
+    setEmails((prevState) => prevState.filter((email) => email.id !== mailId));
+    // mailId = 3
+    // [{id: 1}, {id: 2}, {id: 3}]
+    // [{id: 1}, ] 1 !== 3 // true
+    // [{id: 1}, {id: 2}] 2 !== 3 // true
+    // [{id: 1}, {id: 2}] 3 !== 3 // false
+  };
+
+  const handleToggleMailBox = () => {
+    setShowMailBox((prevState) => !prevState);
+  };
+
   return (
     <div>
-      <MailBox />
+      <h1>Email Counter: {counter}</h1>
+      <button onClick={handleToggleMailBox}>
+        {showMailBox ? "Hide" : "Show"} MailBox
+      </button>
+
+      {showMailBox ? (
+        <MailBox
+          emails={emails}
+          emailCounter={counter}
+          onLogEmail={onLogEmail}
+          onDeleteEmail={handleDelete}
+        />
+      ) : null}
 
       <ProductGallery productData={productData} />
     </div>
--- src/components/MailBox.jsx
+++ src/components/MailBox.jsx
@@ -1,12 +1,29 @@
-const MailBox = () => {
+const MailBox = ({ emails, onLogEmail, onDeleteEmail, emailCounter }) => {
   return (
     <div>
-      <h2>MailBox</h2>
+      <h2>
+        MailBox <b>{emailCounter}</b>
+      </h2>
       <ul>
-        <li>Mail 1</li>
-        <li>Mail 2</li>
-        <li>Mail 3</li>
+        {emails.map((email) => (
+          <li key={email.id}>
+            {email.email}{" "}
+            <button onClick={() => onDeleteEmail(email.id)}>&times;</button>
+          </li>
+        ))}
+        {/* <li>
+          Mail 1 <button onClick={() => onDeleteEmail(1)}>&times;</button>
+        </li>
+        <li>
+          Mail 2 <button onClick={() => onDeleteEmail(2)}>&times;</button>
+        </li>
+        <li>
+          Mail 3 <button onClick={() => onDeleteEmail(3)}>&times;</button>
+        </li> */}
       </ul>
+      <button onClick={onLogEmail} type="button">
+        Send mail
+      </button>
     </div>
   );
 };
